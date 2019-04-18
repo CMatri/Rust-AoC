33d2c8b926b94645de30ae0f24654c1f10e8b587
Connor Perkins <cperkin4@gmu.edu>
2019-04-18 14:58:12 -0400

    Day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,6 @@
 use std::fs;
 
 struct Rect {
-    id: u32,
     x: u32,
     y: u32,
     w: u32,
@@ -12,26 +11,43 @@ fn main() {
     let filename = "day3.txt";
     let file_contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let mut rectangles: Vec<Rect> = Vec::new();
+    let mut grid_width = 0;
+    let mut grid_height = 0;
 
     for line in file_contents.lines() {
         let sub0: Vec<&str> = line.split("@").collect(); // { "#int", "int,int: intxint" }
         let sub1: Vec<&str> = sub0[1].split(":").collect(); // { "int,int", "intxint" }
         let filter = vec!['#', ' '];
-        let id: String = sub0[0].chars().filter(|&c| !filter.contains(&c)).collect();
         let coords: String = sub1[0].chars().filter(|&c| !filter.contains(&c)).collect();
         let xy: Vec<&str> = coords.split(',').collect();
         let dimensions: String = sub1[1].chars().filter(|&c| !filter.contains(&c)).collect();
         let wh: Vec<&str> = dimensions.split('x').collect();
-        rectangles.push(Rect { 
-            id: id.parse().unwrap(), 
+        let rect = Rect { 
             x : xy[0].parse().unwrap(), 
             y : xy[1].parse().unwrap(),
             w : wh[0].parse().unwrap(),
             h : wh[1].parse().unwrap()
-        });
+        };
+
+        if rect.x + rect.w > grid_width { grid_width = rect.x + rect.w; }
+        if rect.y + rect.h > grid_height { grid_height = rect.y + rect.h; }
+
+        rectangles.push(rect);
     }
 
+    let mut grid = vec![vec![0u8; grid_height as usize]; grid_width as usize];
+
     for rect in rectangles {
-        println!("id: {} xy: {}, {} wh: {}, {}", rect.id, rect.x, rect.y, rect.w, rect.h);
+        for x in rect.x..rect.x+rect.w {
+            for y in rect.y..rect.y+rect.h {
+                grid[x as usize][y as usize] += 1;
+            }
+        }
     }
+
+    let mut two_counts = 0;
+    for line in grid {
+        two_counts += line.iter().filter(|&x| *x > 1).count();
+    }
+    println!("{}", two_counts);
 }
